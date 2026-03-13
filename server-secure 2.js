d44ed1ca9735b10b2cc117fced35d4d4f445ab09
Rick Glenn <therickglenn@gmail.com>
2026-03-13 08:05:56 -0700

    chore: cleanup duplicates chunk 3

--- server-secure 2.js
+++
@@ -1,6 +0,0 @@
-// Template secure bootstrap invoking self-test before any server/network listeners.
-const { runStartupSelfTest } = require('./security/selfTest');
-(async () => {
-  await runStartupSelfTest();
-  // Insert real application startup logic here (e.g., require('./app') ).
-})();
